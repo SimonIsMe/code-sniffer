83a0e9f5fad9775086de31af2888d352cd3c818a
szymon <szym1991@gmail.com>
2025-05-08 21:13:06 +0200

    Create ForbiddenTraitsSniff

---
+++ src/Sniffs/ForbiddenTraitsSniff.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Szymon\CodeSniffer\Sniffs;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+final class ForbiddenTraitsSniff implements Sniff
+{
+    public function register()
+    {
+        return [T_TRAIT];
+    }
+
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $error = 'Traits are prohibited (found "%s")';
+        $traitName = $phpcsFile->getDeclarationName($stackPtr);
+        $phpcsFile->addError($error, $stackPtr, 'TraitFound', [$traitName]);
+    }
+}
