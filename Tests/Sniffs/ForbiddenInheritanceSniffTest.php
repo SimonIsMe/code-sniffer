3e45e68cce102000dfb15c99b97ca43646610798
szymon <szym1991@gmail.com>
2025-05-26 16:37:43 +0200

    Support full namespaces

--- Tests/Sniffs/ForbiddenInheritanceSniffTest.php
+++ Tests/Sniffs/ForbiddenInheritanceSniffTest.php
@@ -19,9 +19,15 @@ class ForbiddenInheritanceSniffTest extends TestCase
 
         $this->assertEquals(
             [
-                5 => 'Forbidden inheritance "Children" from "Parent".',
-                7 => 'Forbidden inheritance "SpecificException" from "Exception".',
-                9 => 'Forbidden inheritance "AnotherOne" from "RunException".',
+                5 => 'Forbidden inheritance "\Children" from "\ParentClass".',
+                7 => 'Forbidden inheritance "\SpecificException" from "\Exception".',
+                9 => 'Forbidden inheritance "\AnotherOne" from "\RunException".',
+                16 => 'Forbidden inheritance "\a\b\c\ChildrenA" from "\a\b\c\ParentClass".',
+                18 => 'Forbidden inheritance "\a\b\c\ChildrenB" from "\ParentClass".',
+                28 => 'Forbidden inheritance "\d\e\f\ChildrenA" from "\d\e\f\ParentClass".',
+                30 => 'Forbidden inheritance "\d\e\f\ChildrenB" from "\a\b\c\ParentClass".',
+                32 => 'Forbidden inheritance "\d\e\f\ChildrenC" from "\a\b\c\ParentClass".',
+                34 => 'Forbidden inheritance "\d\e\f\ChildrenD" from "\a\b\c\ParentClass".',
             ],
             $errors,
         );
@@ -35,13 +41,19 @@ class ForbiddenInheritanceSniffTest extends TestCase
             __DIR__ . '/ForbiddenInheritanceSniffTest.inc',
             ForbiddenInheritanceSniff::class,
             [
-                'acceptedParentClassNameRegExpPatterns[]' => '/^(.*)Exception$/',
+                'acceptedParentClassNameWithNamespaceRegExpPatterns[]' => '/^(.*)Exception$/',
             ]
         );
 
         $this->assertEquals(
             [
-                5 => 'Forbidden inheritance "Children" from "Parent".',
+                5 => 'Forbidden inheritance "\Children" from "\ParentClass".',
+                16 => 'Forbidden inheritance "\a\b\c\ChildrenA" from "\a\b\c\ParentClass".',
+                18 => 'Forbidden inheritance "\a\b\c\ChildrenB" from "\ParentClass".',
+                28 => 'Forbidden inheritance "\d\e\f\ChildrenA" from "\d\e\f\ParentClass".',
+                30 => 'Forbidden inheritance "\d\e\f\ChildrenB" from "\a\b\c\ParentClass".',
+                32 => 'Forbidden inheritance "\d\e\f\ChildrenC" from "\a\b\c\ParentClass".',
+                34 => 'Forbidden inheritance "\d\e\f\ChildrenD" from "\a\b\c\ParentClass".',
             ],
             $errors,
         );
@@ -55,14 +67,20 @@ class ForbiddenInheritanceSniffTest extends TestCase
             __DIR__ . '/ForbiddenInheritanceSniffTest.inc',
             ForbiddenInheritanceSniff::class,
             [
-                'acceptedChildClassNameRegExpPatterns[]' => '/^(.*)Exception$/',
+                'acceptedChildClassNameWithNamespaceRegExpPatterns[]' => '/^(.*)Exception$/',
             ]
         );
 
         $this->assertEquals(
             [
-                5 => 'Forbidden inheritance "Children" from "Parent".',
-                9 => 'Forbidden inheritance "AnotherOne" from "RunException".',
+                5 => 'Forbidden inheritance "\Children" from "\ParentClass".',
+                9 => 'Forbidden inheritance "\AnotherOne" from "\RunException".',
+                16 => 'Forbidden inheritance "\a\b\c\ChildrenA" from "\a\b\c\ParentClass".',
+                18 => 'Forbidden inheritance "\a\b\c\ChildrenB" from "\ParentClass".',
+                28 => 'Forbidden inheritance "\d\e\f\ChildrenA" from "\d\e\f\ParentClass".',
+                30 => 'Forbidden inheritance "\d\e\f\ChildrenB" from "\a\b\c\ParentClass".',
+                32 => 'Forbidden inheritance "\d\e\f\ChildrenC" from "\a\b\c\ParentClass".',
+                34 => 'Forbidden inheritance "\d\e\f\ChildrenD" from "\a\b\c\ParentClass".',
             ],
             $errors,
         );
@@ -76,14 +94,14 @@ class ForbiddenInheritanceSniffTest extends TestCase
             __DIR__ . '/ForbiddenInheritanceSniffTest.inc',
             ForbiddenInheritanceSniff::class,
             [
-                'acceptedParentClassNameRegExpPatterns[]' => '/^(.*)Parent/',
-                'acceptedChildClassNameRegExpPatterns[]' => '/^(.*)Exception$/',
+                'acceptedParentClassNameWithNamespaceRegExpPatterns[]' => '/^(.*)Parent/',
+                'acceptedChildClassNameWithNamespaceRegExpPatterns[]' => '/^(.*)Exception$/',
             ]
         );
 
         $this->assertEquals(
             [
-                9 => 'Forbidden inheritance "AnotherOne" from "RunException".',
+                9 => 'Forbidden inheritance "\AnotherOne" from "\RunException".',
             ],
             $errors,
         );
--- src/Standards/Generic/Sniffs/ForbiddenInheritanceSniff.php
+++ src/Standards/Generic/Sniffs/ForbiddenInheritanceSniff.php
@@ -7,9 +7,9 @@ use PHP_CodeSniffer\Sniffs\Sniff;
 
 class ForbiddenInheritanceSniff implements Sniff
 {
-    public array $acceptedChildClassNameRegExpPatterns = [];
+    public array $acceptedChildClassNameWithNamespaceRegExpPatterns = [];
 
-    public array $acceptedParentClassNameRegExpPatterns = [];
+    public array $acceptedParentClassNameWithNamespaceRegExpPatterns = [];
 
     public function register()
     {
@@ -18,22 +18,29 @@ class ForbiddenInheritanceSniff implements Sniff
 
     public function process(File $phpcsFile, $stackPtr)
     {
-        $childClassName = $this->getChildClassName($phpcsFile, $stackPtr);
+        $childClassNameWithNamespace = $this->getChildClassNameWithNamespace($phpcsFile, $stackPtr);
         $isInAcceptedChildClassNamesPatterns = $this->isInPatternArray(
-            $childClassName,
-            $this->acceptedChildClassNameRegExpPatterns,
+            $childClassNameWithNamespace,
+            $this->acceptedChildClassNameWithNamespaceRegExpPatterns,
         );
 
-        $parentClassName = $this->getParentClassNameWithoutNamespace($phpcsFile, $stackPtr);
+        $parentClassNameWithNamespace = $this->getParentClassNameWithNamespace($phpcsFile, $stackPtr);
         $isInAcceptedParentClassNamesPatterns = $this->isInPatternArray(
-            $parentClassName,
-            $this->acceptedParentClassNameRegExpPatterns,
+            $parentClassNameWithNamespace,
+            $this->acceptedParentClassNameWithNamespaceRegExpPatterns,
         );
 
         if ($isInAcceptedChildClassNamesPatterns === false
             && $isInAcceptedParentClassNamesPatterns === false
         ) {
-            $phpcsFile->addError('Forbidden inheritance "%s" from "%s".', $stackPtr, 'aa', [$childClassName, $parentClassName]);
+            $phpcsFile->addError(
+                'Forbidden inheritance "%s" from "%s".', $stackPtr,
+                'aa',
+                [
+                    $childClassNameWithNamespace,
+                    $parentClassNameWithNamespace,
+                ],
+            );
         }
     }
 
@@ -59,14 +66,92 @@ class ForbiddenInheritanceSniff implements Sniff
         ]['content'];
     }
 
-    private function getParentClassNameWithoutNamespace(File $phpcsFile, $stackPtr): string
+    private function getChildClassNameWithNamespace(File $phpcsFile, $stackPtr): string
     {
-        $tokens = $phpcsFile->getTokens();
-        $parentClassNameIndex = $phpcsFile->findPrevious(
-            T_STRING,
-            $phpcsFile->findNext(T_OPEN_CURLY_BRACKET, $stackPtr),
-        );
+        $namespace = $this->getNamespace($phpcsFile, $stackPtr);
+        $className = $this->getChildClassName($phpcsFile, $stackPtr);
 
-        return $tokens[$parentClassNameIndex]['content'];
+        if ($namespace === '') {
+            return '\\' . $className;
+        }
+        return '\\' . $namespace . '\\' . $className;
+    }
+
+    private function getParentClassNameWithNamespace(File $phpcsFile, $stackPtr): string
+    {
+        $namespace = $this->getNamespace($phpcsFile, $stackPtr);
+        $endToken = $phpcsFile->findNext([T_OPEN_CURLY_BRACKET, T_IMPLEMENTS], $stackPtr);
+        $startToken = $phpcsFile->findPrevious(T_EXTENDS, $endToken);
+
+        $parentClassNameIndex = trim($phpcsFile->getTokensAsString($startToken + 1, $endToken - $startToken - 1));
+
+        $explodedParentClassNameIndex = explode('\\', $parentClassNameIndex);
+        $init = array_shift($explodedParentClassNameIndex);
+
+        $imports = $this->getAllImports($phpcsFile, $stackPtr);
+        if (array_key_exists($init, $imports)) {
+            if (!empty($explodedParentClassNameIndex)) {
+                return $imports[$init] . '\\' . implode('\\', $explodedParentClassNameIndex);
+            }
+            return $imports[$init];
+        }
+
+        if (substr($parentClassNameIndex, 0, 1) === '\\') {
+            return $parentClassNameIndex;
+        }
+
+        if ($namespace === '') {
+            return '\\' .  ltrim($parentClassNameIndex, '\\');
+        }
+
+        return '\\' . $namespace . '\\' .  ltrim($parentClassNameIndex, '\\');
+    }
+
+    private function getNamespace(File $phpcsFile, $stackPtr): string
+    {
+        $namespaceTokenPosition = $phpcsFile->findPrevious(T_NAMESPACE, $stackPtr - 1);
+        if ($namespaceTokenPosition === false) {
+            return '';
+        }
+
+        $endTokenPosition = $phpcsFile->findNext(T_SEMICOLON, $namespaceTokenPosition + 1);
+
+        return trim($phpcsFile->getTokensAsString($namespaceTokenPosition + 1, $endTokenPosition - $namespaceTokenPosition - 1));
+    }
+
+    /**
+     * @return string[]
+     */
+    private function getAllImports(File $phpcsFile, int $stackPtr): array
+    {
+        $importsRaw = [];
+
+        do {
+            $useTokenPosition = $phpcsFile->findPrevious(T_USE, $stackPtr - 1);
+            if ($useTokenPosition === false) {
+                break;
+            }
+            $endLinePosition = $phpcsFile->findNext(T_SEMICOLON, $useTokenPosition + 1);
+            $importsRaw[] = trim($phpcsFile->getTokensAsString($useTokenPosition + 1, $endLinePosition - $useTokenPosition - 1));
+
+            $stackPtr = $useTokenPosition - 1;
+        } while (true);
+
+
+        $imports = [];
+        foreach ($importsRaw as $importRaw) {
+            $hasAs = stripos($importRaw, ' as ');
+            if ($hasAs) {
+                $exploded = preg_split('/ as /i', $importRaw);
+                $className = $exploded[1];
+                $imports[$className] = '\\' . $exploded[0];
+            } else {
+                $exploded = explode('\\', $importRaw);
+                $className = end($exploded);
+                $imports[$className] = '\\' . $importRaw;
+            }
+        }
+
+        return $imports;
     }
 }
